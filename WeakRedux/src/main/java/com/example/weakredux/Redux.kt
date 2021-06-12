037f1c5a288c57f79699dc0a62fae70c0689534e
Brylle <kiirbritmiin@gmail.com>
2021-06-12 22:43:12 +0800

    Restructure most of the library to prevent overriding of dispatch function

--- WeakRedux/src/main/java/com/example/weakredux/Redux.kt
+++ WeakRedux/src/main/java/com/example/weakredux/Redux.kt
@@ -1,19 +1,23 @@
 package com.example.weakredux
 
 interface Redux <S, A> where S: State, A: Action {
-    val store: Store<S>
-    val reducer: (S, A) -> S
+    val store: Store<S, A>
     fun update(newState: S)
 
-    fun dispatch(vararg actions: A) {
-        actions.forEach { action ->
-            store.state = reducer(store.state, action)
-        }
-        update(store.state)
-    }
+    class Store<S, A>(initialState: S,
+                      private val reducer: (S, A) -> S,
+                      private val implementer: Redux<S, A>) where S: State, A: Action {
+        private var state = initialState
 
-    class Store<S>(initialState: S) where S: State {
-        internal var state = initialState
+        fun dispatch(vararg actions: A) {
+            actions.forEach { action ->
+                this.state = reducer(this.state, action)
+            }
+
+            implementer.update(this.state)
+        }
+
+        fun getState() = state
     }
 }
 
--- app/src/main/java/com/example/weakreduxexample/MainActivity.kt
+++ app/src/main/java/com/example/weakreduxexample/MainActivity.kt
@@ -9,6 +9,7 @@ import com.example.weakredux.Action
 import com.example.weakredux.Redux
 import com.example.weakredux.State
 import com.google.android.material.textfield.TextInputLayout
+import kotlin.reflect.KClass
 
 class MainActivity: AppCompatActivity(), Redux<MainActivity.SampleState, MainActivity.SampleAction> {
     private val inputUsername: TextInputLayout by lazy { findViewById(R.id.input_username) }
@@ -19,44 +20,53 @@ class MainActivity: AppCompatActivity(), Redux<MainActivity.SampleState, MainAct
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        /** Call the dispatch()'es on UI interactions */
+        /**
+         * Call the store dispatch() on UI interactions.
+         */
         inputUsername.editText?.doAfterTextChanged { editable ->
-            this.dispatch(SampleAction.SetUsername(username = editable.toString()))
+            store.dispatch(SampleAction.SetUsername(username = editable.toString()))
         }
         inputPassword.editText?.doAfterTextChanged { editable ->
-            this.dispatch(SampleAction.SetPassword(password = editable.toString()))
+            store.dispatch(SampleAction.SetPassword(password = editable.toString()))
         }
     }
 
-    /** These can be written someplace else */
+    /**
+     * This block can be written someplace else, and separately.
+     */
     data class SampleState(val usernameInput: String, val passwordInput: String): State
     sealed class SampleAction: Action {
         data class SetUsername(val username: String): SampleAction()
         data class SetPassword(val password: String): SampleAction()
     }
-
-    private val initialState = SampleState(usernameInput = "", passwordInput = "")
-    override val store: Redux.Store<SampleState> = Redux.Store(initialState)
-
-    /**
-     * Define the reducer.
-     * It's commonly just shallow copies of members of (a presumably flat) State
-     */
-    override val reducer: (SampleState, SampleAction) -> SampleState = { oldState, action ->
-        // can also add a logger here
+    private val reducer: (SampleState, SampleAction) -> SampleState = { oldState, action ->
+        /** can also add a logger here */
         Log.d("my tag", "$action")
         when (action) {
             is SampleAction.SetUsername -> oldState.copy(usernameInput = action.username)
             is SampleAction.SetPassword -> oldState.copy(passwordInput = action.password)
         }
     }
+    private val initialState = SampleState(usernameInput = "", passwordInput = "")
 
     /**
-     * Put View logic here. Avoid infinite loops by dispatching Actions inside
+     * Initialize store with an initial state, a reducer, and the activity reference that implements
+     * Redux (this).
+     *
+     * The reference is needed so update() is called after every call to dispatch().
+     */
+    override val store: Redux.Store<SampleState, SampleAction> = Redux.Store(initialState, reducer, this)
+
+    /**
+     * Called after every dispatch().
+     *
+     * Put View logic here. In general, don't call action dispatches here to avoid infinite loops.
+     * The UI element most prone to infinite loops is EditText's afterTextChanged() callback.
      */
     override fun update(newState: SampleState) {
-        // some sample logic: enable button once both inputs are not empty (not checking for blanks, though)
+        /** some sample logic: enable button once both inputs are not empty (not checking for blanks, though) */
         button.isEnabled = newState.usernameInput != "" && newState.passwordInput != ""
     }
+
 
 }
